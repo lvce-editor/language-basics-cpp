77b9abd258d2f705cc0f846e1193a7014f57bbf4
Le Vivilet <levivlet@pm.me>
2023-07-03 17:11:48 +0200

    test: add more tests from node-pty

---
+++ scripts/copy-node-pty-tests.js
@@ -0,0 +1,60 @@
+import { execaCommand } from 'execa'
+import { readFile, readdir, rm, writeFile } from 'node:fs/promises'
+import path, { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const root = path.join(__dirname, '..')
+
+const REPO = 'https://github.com/microsoft/node-pty'
+const COMMIT = '5278274ee2bfa2b8325d3ee7595ea0842a77d0d7'
+
+const getTestName = (line) => {
+  return (
+    'node-pty-' +
+    line
+      .toLowerCase()
+      .trim()
+      .replaceAll(' ', '-')
+      .replaceAll('/', '-')
+      .replace('.cc', '')
+  )
+}
+
+const getAllTests = async (folder) => {
+  const dirents = await readdir(folder, { recursive: true })
+  const allTests = []
+  for (const dirent of dirents) {
+    console.log({ dirent })
+    if (!dirent.endsWith('.cc')) {
+      continue
+    }
+    const filePath = join(folder, dirent)
+    const fileContent = await readFile(filePath, 'utf8')
+    const testName = getTestName(dirent)
+    allTests.push({
+      testName,
+      testContent: fileContent,
+    })
+  }
+  return allTests
+}
+
+const writeTestFiles = async (allTests) => {
+  for (const test of allTests) {
+    await writeFile(`${root}/test/cases/${test.testName}.cpp`, test.testContent)
+  }
+}
+
+const main = async () => {
+  process.chdir(root)
+  await rm(`${root}/.tmp`, { recursive: true, force: true })
+  await execaCommand(`git clone ${REPO} .tmp/node-pty`)
+  process.chdir(`${root}/.tmp/node-pty`)
+  await execaCommand(`git checkout ${COMMIT}`)
+  process.chdir(root)
+  const allTests = await getAllTests(`${root}/.tmp/node-pty/src`)
+  await writeTestFiles(allTests)
+}
+
+main()
